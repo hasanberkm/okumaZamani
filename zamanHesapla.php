233a2f442741afc692f3cf7e1476ef76b167d0d0
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2017-07-26 17:22:50 +0300

    Update zamanHesapla.php

--- zamanHesapla.php
+++ zamanHesapla.php
@@ -1,27 +1,27 @@
-﻿<?php
+<?php
 	function zamanHesapla($veri = "", $saniyelikKelime = 2)
 	{
-		$kelimeSayısı = round(count(explode(" ", $veri))); //Content Değişkenindeki İçeriğin Kelime Sayısını Bulup Tutar.
-		$okumaZamanı = ceil($kelimeSayısı++ / $saniyelikKelime); //Okuma Süresini Hesaplayıp Tutar.
+		$kelimeSayisi = round(count(explode(" ", $veri))); //Content Değişkenindeki İçeriğin Kelime Sayısını Bulup Tutar.
+		$okumaZamani = ceil(++$kelimeSayisi / $saniyelikKelime); //Okuma Süresini Hesaplayıp Tutar.
 		
 		//Eğer $readingTime 60 Saniyeden Az İse (Yani 1 Dakikadan Az İse) Süreyi Saniye Cinsinden Bastırır.
-		if ($okumaZamanı < 60)
+		if ($okumaZamani < 60)
 		{
-			$saniye = $okumaZamanı;
+			$saniye = $okumaZamani;
 			echo "$saniye Saniyede Okuyabilirsiniz.";
 		}
 		
 		//Eğer 3600'den Az İse (Yani 1 Saatten Az İse) Süreyi Dakika Cinsinden Bastırır.
-		else if ($okumaZamanı < 3600)
+		else if ($okumaZamani < 3600)
 		{
-			$dakika = ceil($okumaZamanı / 60);
+			$dakika = ceil($okumaZamani / 60);
 			echo "$dakika Dakikada Okuyabilirsiniz";
 		}
 		
 		//Eğer 3600 ve üzeriyse Süreyi Saat Cinsinden Bastırır
 		else
 		{
-			$saat = floor($okumaZamanı / 3600);
+			$saat = floor($okumaZamani / 3600);
 			echo "$saat Saatte Okuyabilirsiniz";
 		}
 	}
